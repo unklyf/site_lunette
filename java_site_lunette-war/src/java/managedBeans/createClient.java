ecb00322ede0860235854afccdb7c7c9960f97db
Home <Home@LoicPC.lan>
2013-11-26 18:40:28 +0100

    createcli

---
+++ java_site_lunette-war/src/java/managedBeans/createClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBeans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Home
+ */
+@Stateless
+@LocalBean
+public class createClient {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
