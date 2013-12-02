43aec7d241fed442ae689eedfcb8be07d97e9859
Unklyf <Unklyf@Black-Pearl.lan>
2013-12-02 18:48:27 +0100

    managedBeans

---
+++ java_site_lunette-war/src/java/managedBeans/Categorie.java
@@ -0,0 +1,26 @@
+package managedBeans;
+
+import java.util.List;
+import javax.ejb.EJB;
+import model.Produit;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import sessionBeans.ProduitFacadeLocal;
+
+/**
+ *
+ * @author Unklyf
+ */
+@ManagedBean (name="categorie")
+@SessionScoped
+public class Categorie {
+
+    @EJB
+    private ProduitFacadeLocal prodFacade;   
+    
+    public List<Produit> prodList(){
+        List<Produit> prod = prodFacade.findAll();
+        return prod;
+    }
+   
+}
